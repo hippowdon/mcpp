a3a6b7fdda06e44f3fd55a10c12ea44fb2e757d8
Aidar Talibzhanov <aidar.talibzhanov@yahoo.com>
2019-11-10 20:11:02 +0000

    exercise driver, exercise static library, Catch2 unit test driver

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+//
+// Exercise driver
+//
+//
 
-int main(int argc, char **argv) {
+#include <iostream>
+#include "ex1.h" // an exercise header
+
+int main(int argc, char **argv) 
+{
     std::cout << "Hello, world!" << std::endl;
+    std::cout << mcpp::foo() << std::endl;
     return 0;
 }
